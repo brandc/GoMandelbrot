3cc5515331d98f11bb3d16c93b57bfa2f5d5ec18
brandc <1051715+brandc@users.noreply.github.com>
2024-07-25 05:52:33 -0600

    Modified mandelbrot function to execute concurrently

--- main.go
+++ main.go
@@ -9,14 +9,23 @@ import (
 	"math/cmplx"
 	"os"
 	"fmt"
+	"runtime"
+	"bufio"
 )
 
 var colors []color.Color
 var mMin float64 = -2.0
 var mMax float64 =  2.0
+var sema chan struct{}
+
+type Frame struct {
+	Imgseq int
+	Img *image.Paletted
+}
 
 func init() {
 	colors = palette.Plan9
+	sema = make(chan struct{}, runtime.NumCPU())
 }
 
 func main() {
@@ -28,7 +37,7 @@ func coordinateTranslate(x, srcMin, srcMax, destMin, destMax float64) (ret float
 	return (((x - srcMin) / (srcMax - srcMin)) * (destMax - destMin)) + destMin
 }
 
-func mandelbrotFrame(dStartX, dEndX, dStartY, dEndY, nPow float64, iterMax, imgW, imgH int) *image.Paletted {
+func mandelbrotFrame(dStartX, dEndX, dStartY, dEndY, nPow float64, iterMax, imgW, imgH, imgseq int, ch chan *Frame) {
 	// Number of fractal formula iterations
 	var iter int
 	// z[n+1] = z[n]^2 + c
@@ -38,13 +47,18 @@ func mandelbrotFrame(dStartX, dEndX, dStartY, dEndY, nPow float64, iterMax, imgW
 	// Translated x and y
 	var tx, ty float64
 
+	var ret Frame
+	ret.Imgseq = imgseq
+
+	sema <- struct{}{}
+
 	rect  := image.Rect(0, 0, imgW, imgH)
 	frame := image.NewPaletted(rect, colors)
 
 	for y := 0; y < imgH; y++ {
-		ty = coordinateTranslate(float64(y), 0.0, float64(imgH), dStartY, dEndY)
+		ty = coordinateTranslate(float64(y), 0.0, float64(imgH) - 1, dStartY, dEndY)
 		for x := 0; x < imgW; x++ {
-			tx = coordinateTranslate(float64(x), 0.0, float64(imgW), dStartX, dEndX)
+			tx = coordinateTranslate(float64(x), 0.0, float64(imgW) - 1, dStartX, dEndX)
 
 			c = complex(tx, ty)
 			z = complex(0, 0)
@@ -55,32 +69,64 @@ func mandelbrotFrame(dStartX, dEndX, dStartY, dEndY, nPow float64, iterMax, imgW
 			frame.SetColorIndex(x, y, uint8(iter % len(colors)))
 		}
 	}
+	ret.Img = frame
 
-	return frame
+	<-sema
+	ch <- &ret
 }
 
 // Mandelbrot set:
 // f[c] = z^2 + c
 func mandelbrotAnimation(out io.Writer) {
-	imageH := 2000
-	imageW := 2000
+	const (
+		// Image
+		imageH = 2000
+		imageW = 2000
 
-	frameTotal := 1000
-	frameDelay := 8
+		// gif
+		frameTotal = 1000
+		frameDelay = 20
 
-	iterMax := 100
+		// Mandelbrot
+		iterMax = 100
+	)
+
+	var nPow float64
+
+	var frames []*Frame
 
 	animation := gif.GIF{LoopCount: frameTotal}
 
-	var nPow float64
+	ch := make(chan *Frame)
+
 	for i := 0; i < frameTotal; i++ {
 		nPow = 2.0 + 10.0 * (float64(i) / float64(frameTotal))
+		go mandelbrotFrame(mMin, mMax, mMin, mMax, nPow, int(iterMax), int(imageW), int(imageH), i, ch)
+	}
 
-		fmt.Fprintf(os.Stderr, "frame: %d\n", i+1)
-		frame := mandelbrotFrame(mMin, mMax, mMin, mMax, nPow, int(iterMax), int(imageW), int(imageH))
+	w := bufio.NewWriter(os.Stderr)
+	for ;len(frames) < frameTotal; {
+		f := <-ch
+		frames = append(frames, f)
+		fmt.Fprintf(w, "\rFrame: %4d / %d", len(frames), frameTotal)
+		w.Flush()
+	}
+	fmt.Fprintln(w)
 
+	for i := 0; i < len(frames); i++ {
+		for j := i+1; j < len(frames); j++ {
+			if frames[i].Imgseq > frames[j].Imgseq {
+				tmp := frames[i]
+				frames[i] = frames[j]
+				frames[j] = tmp
+			}
+		}
+	}
+
+
+	for i := 0; i < frameTotal; i++ {
 		animation.Delay = append(animation.Delay, frameDelay)
-		animation.Image = append(animation.Image, frame)
+		animation.Image = append(animation.Image, frames[i].Img)
 	}
 
 	gif.EncodeAll(out, &animation)
